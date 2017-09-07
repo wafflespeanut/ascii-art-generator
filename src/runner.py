030a6d844ead364036989338440f58aebbcf1d2f
Ravi Shankar <wafflespeanut@gmail.com>
2017-09-08 01:05:34 +0530

    Disable debug and threads

--- src/runner.py
+++ src/runner.py
@@ -149,4 +149,4 @@ if __name__ == '__main__':
 
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0', port=port)
